d44bd63c1bf095dec06e4b75c34ecb647ac7bdd5
V-Rod <pjvelazquez@me.com>
2016-11-03 17:27:47 -0500

    JavaScript: -introduced JS today

---
+++ js/if_else.js
@@ -0,0 +1,53 @@
+/**
+ * Created by V-Rod on 11/3/16.
+ */
+"use strict";
+
+/*var studentgrade = (70 + 80 + 95)/3;
+
+
+
+if (studentgrade >= 80) {
+    console.log("Operation was successful");
+} else {
+    console.log("Oops, something went wrong.  Try again!")
+}
+
+
+
+var flipACoin = Math.floor(Math.random()* 2);
+
+if (flipACoin > 0) {
+    console.log("Buy a house");
+} else{
+    console.log("Buy a car")
+};*/
+
+
+/*var flipACoin = Math.floor(Math.random()* 2);*/
+
+var flipACoin > (0) ? "Buy a house" : "Buy a car" ;
+console.log();
+
+/*var ryan = 250;
+var cameron = 180;
+var george = 320;
+var discount = 200;
+
+if (ryan>=discount) {
+    document.write("Ryan has spent $" + (ryan-discount) + " over the required amount of $200 and has earned a discount")
+} else if (ryan<discount) {
+    document.write(" Ryan needs to spend $" + (discount - ryan) + " to earn a discount.")
+};
+
+if (cameron >= discount) {
+    document.write("Cameron has spent $" + (cameron - discount) + " over the required amount of $200 and has earned a discount")
+} else if (cameron < discount) {
+    document.write("Cameron needs to spend $" + (discount - cameron) + " to earn a discount.")
+};
+
+if (george >= discount) {
+    document.write("George has spent $" + (george - discount) + " over the required amount of $200 and has earned a discount")
+} else if (george < discount) {
+    document.write("George needs to spend $" + (discount - george) + " to earn the discount.")
+};*/
