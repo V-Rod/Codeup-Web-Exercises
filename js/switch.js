f7ef76e2988cf7442b38fc56d65e0abb8e905bea
V-Rod <pjvelazquez@me.com>
2016-11-04 10:39:08 -0500

    New things added today: -Switch Statements -What If Statements

---
+++ js/switch.js
@@ -0,0 +1,73 @@
+/**
+ * Created by V-Rod on 11/4/16.
+ */
+"use strict";
+
+/*var luckyNumber = Math.floor(Math.random() * 6);
+console.log(luckyNumber);
+
+var total = 60;
+var discountPercent = 0;
+var discount;
+
+//THIS IS WHAT I WROTE WHICH WAS WRONG
+ /*switch (luckyNumber) {
+ case 0:
+ console.log("You get no discount");
+ break;
+ case 1:
+ console.log("You get 10% discount");
+ break;
+ case 2:
+ console.log("You get 25% discount");
+ break;
+ case 4:
+ console.log("You get 50% discount");
+ break;
+ case 5:
+ console.log("You will get it for free");
+ break;
+ default:
+ console.log("You get no discount");
+ }*/
+
+ /*switch (luckyNumber) {
+     case 1:
+         discountPercent = 0.1;
+         break;
+     case 2:
+         discountPercent = 0.25;
+         break;
+     case 4:
+         discountPercent = 0.5;
+         break;
+     case 5:
+         discountPercent = 1;
+         break;
+     default:
+         discountPercent = 0;
+ }
+ //apply the discontPercent
+discount = (total * discountPercent);
+console.log("You will pay " + (total - discount));
+console.log("Your original price: " + total);
+console.log("Your lucky number was " + luckyNumber);
+console.log("You get a discount of " + (discountPercent));
+console.log("You saved $" +  discount + ".");
+console.log("You will pay $" + (total - discount));*/
+
+
+var month = 2;
+
+switch(month) {
+    case 1:
+        console.log('January');
+        break;
+    case 2:
+        console.log('February')
+        break;
+    case 3:
+        console.log('March');
+        break;
+    //etc etc etc DO NOT WRITE STUFF ON NOTES AND THEN COPY PASTE HERE IT WILL GIVE YOU SYNTAX ERRORS//
+}
