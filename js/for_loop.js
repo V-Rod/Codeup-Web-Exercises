b6dd596ecfe22afa56193b6c05bb5b7b7d3fc26b
V-Rod <pjvelazquez@me.com>
2016-11-07 01:27:17 -0600

    New things added today: -for loop

---
+++ js/for_loop.js
@@ -0,0 +1,16 @@
+/**
+ * Created by V-Rod on 11/7/16.
+ */
+'use strict';
+
+/*for (var i = 0; i < 10; i++) {
+    console.log('for loop iteration #' + i);
+}
+
+for (var x = 100; x >= 5; x -=5) {
+    console.log(x);
+}*/
+
+for (var y = Math.floor(Math.random()*10); y < 10; y *= 2) {
+    console.log(y + 1);
+}
