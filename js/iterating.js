a5bbbf4649357d1d92809be8285a154868beb681
V-Rod <pjvelazquez@me.com>
2016-11-09 14:08:02 -0600

    3.5 Arrays : Iterating

---
+++ js/iterating.js
@@ -0,0 +1,23 @@
+/**
+ * Created by V-Rod on 11/9/16.
+ */
+(function(){
+    'use strict';
+
+    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+
+    var names = ['Fernando', 'Zach', 'Dimitri', 'Jason'];
+
+    // TODO: Create a log statement that will log the number of elements in the names array.
+
+    console.log('The length of this array is ' + names.length + '.');
+
+    // TODO: Create log statements that will print each of the names array elements individually.
+
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+
+
+})();
