ce53fe3d1d5db6b07d189934f4080edc9461d3b9
V-Rod <pjvelazquez@me.com>
2016-11-11 11:07:34 -0600

    3.6.1 Objects exeecise

---
+++ js/objects_json_js.js
@@ -0,0 +1,79 @@
+/**
+ * Created by V-Rod on 11/10/16.
+ */
+
+'use strict';
+
+// todo:
+// Create an array of objects that represent books.
+// Each book should have a title and an author.
+// The book author should be an object with a firstName and lastName.
+// Be creative and add at least 5 books to the array
+ var books = [
+     {
+         book1 : {
+             title: 'Pride & Prejudice',
+             author: {
+                 firstName: 'Jane',
+                 lastName: 'Austen'
+             }
+         },
+
+         book2 : {
+             title : 'Treasure Island',
+             author : {
+                 firstName : 'Robert Louis',
+                 lastName : 'Stevenson'
+             }
+         },
+
+         book3 : {
+             title : 'Dracula',
+             autor : {
+                 firstName : 'Bram',
+                 lastName : 'Stocker'
+             }
+         },
+
+         book4 : {
+             title : 'War and Peace',
+             author : {
+                 firstName: 'Leo',
+                 lastName : 'Tolstoy'
+             }
+         },
+
+         book5 : {
+             title : 'JavaScript : The Good Parts',
+             author : {
+                 firstName : 'Douglas',
+                 lastName : 'Crockford'
+             }
+         }
+
+     }
+
+ ];
+
+// log out the books array
+// console.log(books);
+console.log(books);
+
+
+
+
+// todo:
+// Loop through the array of books using .forEach and print out the specified information about each one.
+// start loop here
+
+//books.forEach(function (book) {
+  //      console.log('Book # ' + book);
+  //  });
+
+
+//console.log("Title: " + todo);
+//console.log("Author: " + todo);
+//console.log("---");
+// end loop here
+
+
